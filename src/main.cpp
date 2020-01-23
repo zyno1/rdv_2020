88c966c179f7e2cf58353fd241b14c8ef8e7de46
zyno1 <olivierzeyen@gmail.com>
2020-01-23 15:38:16 +0100

    center duck coord

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,34 @@ void render(Model const& model, Matrix const& matrix, Pixmap& pixmap) {
 
     float posCalc = std::min(pixmap.getW(), pixmap.getH()) / 2;
 
-    for(size_t i = 0; i < model.getNbVertices(); i++) {
-        Vector tmp = matrix * model.getVertice(i);
-        tmp *= posCalc;
+    for(size_t i = 0; i < model.getNbFaces(); i++) {
+        std::vector<Vector> v;
+        for(size_t j = 0; j < 3; j++) {
+            v.push_back(matrix * model.getVertice(i, j));
+        }
 
-        int x = tmp.x + posCalc;
-        int y = tmp.y + posCalc;
+        Vector min = v[0];
+        Vector max = v[0];
 
-        if(x >= 0 && x < pixmap.getW() && y >= 0 && y < pixmap.getH()) {
-            for(int i = 0; i < 3; i++) {
-                for(int j = 0; j < 3; j++) {
-                    int tmpx = std::max(0, std::min((int)pixmap.getW() - 1, x + i));
-                    int tmpy = std::max(0, std::min((int)pixmap.getH() - 1, y + j));
+        for(size_t j = 1; j < v.size(); j++) {
+            min.x = std::min(min.x, v[j].x);
+            min.y = std::min(min.y, v[j].y);
 
-                    pixmap.setPixel(tmpx, tmpy, color);
-                }
+            max.x = std::max(max.x, v[j].x);
+            max.y = std::max(max.y, v[j].y);
+        }
+
+        min = (min * posCalc) + Vector(posCalc, posCalc, 0);
+        max = (max * posCalc) + Vector(posCalc, posCalc, 0);
+
+        size_t minx = (size_t)std::max(0, std::min((int)pixmap.getW(), (int)min.x));
+        size_t miny = (size_t)std::max(0, std::min((int)pixmap.getH(), (int)min.y));
+        size_t maxx = (size_t)std::max(0, std::min((int)pixmap.getW() - 1, (int)max.x));
+        size_t maxy = (size_t)std::max(0, std::min((int)pixmap.getH() - 1, (int)max.y));
+
+        for(size_t y = miny; y < maxy; y++) {
+            for(size_t x = minx; x < maxx; x++) {
+                pixmap.setPixel(x, y, color);
             }
         }
     }
@@ -42,7 +55,7 @@ int main() {
 
     m = Matrix::zoom(0.2f) * m;
     //m = Matrix::rotateZ(-2.f) * m;
-    m = Matrix::translate(-1.0f, 0, 0) * m;
+    //m = Matrix::translate(-1.0f, 0, 0) * m;
 
     std::cout << model << std::endl;
 
